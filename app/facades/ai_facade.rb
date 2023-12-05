68b2aef24026df40f7178ce342c13d2e95300f74
Nick Spencer <strangebrew202@gmail.com>
2023-12-05 14:10:15 -0700

    Add: Migrated our OpenAI model to gpt-3.5-turbo-1106, this gives us far faster response times and access to the response_format:JSON attribute improving the consistency of our results

--- app/facades/ai_facade.rb
+++ app/facades/ai_facade.rb
@@ -20,7 +20,7 @@ class AiFacade
     categories = params.keys
     tech_needs = params[categories.first][:tech_needs].join(", ")
     disability_description = params[categories.first][:disability_description].first
-    "Finish your though, but please provide a cross-platform list of #{tech_needs} for people who #{disability_description}. Please provide a definition of each tool and a website for further exploration. Format your response as a valid JSON object, with each tech and it's information being a separate nested object in the technologies array like so {'technologies'=>[{'name'=>'tool_name', 'definition'=>'tool_definition', 'website'=>'tool_website'}]}"
+    "Finish your thought, but please provide a cross-platform list of #{tech_needs} for people who #{disability_description}. Please provide a definition of each tool and a website for further exploration. Format your whole response as a valid JSON object, with each tech and it's information being a separate nested object in the technologies array like so {'technologies'=>[{'name'=>'tool_name', 'definition'=>'tool_definition', 'website'=>'tool_website'}]}"
   end
 
 end
--- app/services/ai_service.rb
+++ app/services/ai_service.rb
@@ -16,7 +16,8 @@ class AiService
 
   def chat_to_ai(message)
     payload = {
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-1106",
+      response_format: { "type": "json_object" },
       max_tokens: 700,
       messages: [
         {
